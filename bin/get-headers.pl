7b86a67a96f825bf4350b7d8568790375dab11d9
Martin Falkus <martin@falkus.co>
2016-04-02 08:43:20 +0100

    Add some docs for Fetcher, get-headers and overall README

--- bin/get-headers.pl
+++ bin/get-headers.pl
@@ -95,3 +95,57 @@ sub show_results {
     print 'Status Code: ' . $code . "\n";
     print "Headers: " . Dumper($headers) . "\n";
 }
+
+
+=head1 NAME
+
+get-headers.pl - Retrieve and store HTTP headers
+
+=head1 SYNOPSIS
+
+    cat 'my-list-of-sites' | bin/gather-headers
+
+=head1 DESCRIPTION
+
+This script will make a HEAD request to each URL passed in on STDIN,
+then store the results in a database, as defined in C<etc/config.yaml>.
+
+=head2 Arguments
+
+=over
+
+=item C<a|alexa>
+
+Correctly parses the alexa file input, which is a CSV of the format
+C<rank,site>. This will then store the site, rank and source in the database.
+
+=item C<b|batch>
+
+How many URLs should be requested at once before blocking until they
+have all either returned a result or timed-out. This value defaults
+to 100.
+
+=item C<u|url=s>
+
+Provide a single URL to retrieve, print out the results rather than
+store the information in the database.
+
+=item C<v>
+
+Print each URL requested along with the returned status code and whether all
+the headers were successfully added to the database. A very large returned
+header value could be truncated when inserted, resulting in what will be
+considered as an unsuccessful insert.
+
+=back
+
+=head1 NOTES
+
+The perldoc for the C<PopularHeaders/Fetcher.pm> module explains the specifics
+used for fetching with regards to how redirects are handled.
+
+=head1 AUTHOR
+
+Martin Falkus <http://falkus.co/>
+
+=cut
--- lib/PopularHeaders/Fetcher.pm
+++ lib/PopularHeaders/Fetcher.pm
@@ -43,3 +43,56 @@ sub get_url_header {
 }
 
 1;
+
+=head1 NAME
+
+PopularHeaders::Fetcher - Non-blocking HEAD request to provided site
+
+=head1 SYNOPSIS
+
+    use PopularHeaders::Fetcher;
+    my $fetcher = PopularHeaders::Fetcher->new();
+
+    $fetcher->get_url_header({
+        'site'      => $url
+        'source'    => 'Alexa',
+        'rank'      => 1000
+    }, $delay, \&store_results_callback);
+
+=head1 DESCRIPTION
+
+
+=head2 Methods
+
+=over
+
+=item C<new>
+
+Returns a new PopularHeaders::Fetcher object which can then be used to make
+non-blocking HEAD requests.
+
+The maximum amount of redirects that will be automatically followed is 3.
+The default timeouts are:
+
+    connect_timeout     => 3 seconds
+    inactivity_timeout  => 10 seconds
+    request_timeout     => 3 seconds
+
+=item C<get_url_header>
+
+Perform a non-blocking HEAD request to the given site.  The supplied hash must
+provide at least a 'site' value to be fetched. The second argument is the
+Mojo::IOLoop Delay instance, and the last value is the callback to be used
+once the request has returned.
+
+The callback is passed 3 arguments. The first is the supplied target hash, the
+second is the final HTTP status code, and the last is a hash of the headers
+received.
+
+=back
+
+=head1 AUTHOR
+
+Martin Falkus <http://falkus.co/>
+
+=cut
